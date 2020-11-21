942be68177eb700d4240fe77730ccb41e006bc30
GyuPark <gyutie@gmail.com>
2020-11-21 13:32:22 +0900

    Create 20201121 Codgm Onboarding.js
    Started working on Onboarding

---
+++ 20201121 Codgm Onboarding.js
@@ -0,0 +1,20 @@
+/**
+ * CodinGame planet is being attacked by slimy insectoid aliens.
+ * <---
+ * 매 loop 마다 플레이어로부터 가장 가까운 2 에너미와 그 거리를 제공한다. 
+ * loop의 마지막에 2 에너미 중 가장 가까운 놈을 출력하면 된다.
+ **/
+
+
+// game loop
+while (true) {
+    const enemy1 = readline(); // name of enemy 1
+    const dist1 = parseInt(readline()); // distance to enemy 1
+    const enemy2 = readline(); // name of enemy 2
+    const dist2 = parseInt(readline()); // distance to enemy 2
+
+    // dist1과 dist2를 비교한다
+    // 가까운 enemy1를 출력한다.
+    // You have to output a correct ship name to shoot ("Buzz", enemy1, enemy2, ...)
+    console.log('name of the enemy');
+}
